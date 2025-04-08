43ba2c63be45342a54bb61096bcb50f6efae29cd
ABGEO <takalandzet@gmail.com>
2025-04-08 17:46:52 +0400

    refactor(ui): extract layouts to layout directory

--- apps/ui/src/app/dashboard/layout.tsx
+++ apps/ui/src/app/dashboard/layout.tsx
@@ -1,72 +1,13 @@
 import { ReactNode } from 'react';
-import { redirect } from 'next/navigation';
 
-import { Content, Footer, Header } from 'antd/lib/layout/layout';
-import { Flex, Layout } from 'antd';
-import Title from 'antd/lib/typography/Title';
-
-import { AccountDropdown } from '@self/components/AccountDropdown';
-import { ApiFactoryProvider } from '@self/providers/ApiFactoryProvider';
-import { AppBreadcrumb } from '@self/components/AppBreadcrumb/AppBreadcrumb';
-import { AppSider } from '@self/components/AppSider';
-import { AuthProvider } from '@self/providers/AuthProvider';
-import { Logo } from '@self/components/Logo';
-import { auth } from '@self/lib/auth';
-
-import rootClasses from '../(root)/Layout.module.css';
-
-import classes from './Layout.module.css';
+import { DashboardLayout as Layout } from '@self/layout/DashboardLayout';
 
 type DashboardLayoutProps = {
   children: ReactNode;
 };
 
 async function DashboardLayout({ children }: DashboardLayoutProps) {
-  const session = await auth();
-
-  if (!session) {
-    redirect('/');
-  }
-
-  return (
-    <ApiFactoryProvider>
-      <AuthProvider>
-        <Layout className={rootClasses.rootLayout}>
-          <Header className={`${rootClasses.header} ${classes.header}`}>
-            <Flex
-              className={rootClasses.headerChildren}
-              align="center"
-              justify="space-between"
-            >
-              <Flex align="center">
-                <Logo width="3em" height="3em" />
-
-                <Title
-                  className={`${rootClasses.appName} ${classes.appName}`}
-                  level={4}
-                >
-                  Follytics
-                </Title>
-              </Flex>
-              <AccountDropdown />
-            </Flex>
-          </Header>
-          <Layout hasSider>
-            <AppSider />
-            <Layout className={classes.contentLayout}>
-              <AppBreadcrumb />
-              <Content className={`${rootClasses.content} ${classes.content}`}>
-                {children}
-              </Content>
-              <Footer className={`${rootClasses.footer} ${classes.footer}`}>
-                Follytics ©2025-{new Date().getFullYear()}
-              </Footer>
-            </Layout>
-          </Layout>
-        </Layout>
-      </AuthProvider>
-    </ApiFactoryProvider>
-  );
+  return <Layout>{children}</Layout>;
 }
 
 export default DashboardLayout;
