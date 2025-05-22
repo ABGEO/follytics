5e720809c6c49ba500f433b356cffc17990e474a
ABGEO <takalandzet@gmail.com>
2025-05-22 19:47:10 +0400

    fix(chart): fix axis generation

--- libs/chart/src/components/axis.ts
+++ libs/chart/src/components/axis.ts
@@ -9,30 +9,19 @@ function addAxis(
   config: ChartConfiguration,
   xScale: d3.ScaleTime<number, number>,
   yScale: d3.ScaleLinear<number, number>,
-  margins: { left: number; bottom: number }
+  margins: { left: number; bottom: number },
 ) {
   const { height = DEFAULTS.height } = config;
 
   svg
     .append('g')
     .attr('transform', `translate(0,${height - margins.bottom})`)
-    .call(
-      d3
-        .axisBottom<Date>(xScale)
-        .ticks(d3.timeDay.every(1))
-        .tickFormat(d3.timeFormat('%d %b'))
-        .tickSizeOuter(0)
-    );
+    .call(d3.axisBottom<Date>(xScale).tickFormat(xScale.tickFormat()));
 
   svg
     .append('g')
     .attr('transform', `translate(${margins.left},0)`)
-    .call(
-      d3
-        .axisLeft(yScale)
-        .ticks(height / 50)
-        .tickFormat(d3.format('d'))
-    );
+    .call(d3.axisLeft<number>(yScale).tickFormat(yScale.tickFormat()));
 }
 
 export { addAxis };
